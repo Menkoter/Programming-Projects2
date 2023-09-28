f15a9f2c62d595f7e9442efca8e2444e360ee629
Menkoter <9018140@student.zadkine.nl>
2023-09-28 10:22:56 +0200

    done

---
+++ src/components/Book.js
@@ -0,0 +1,18 @@
+
+const Book = () => {
+  return (
+    <div>
+      <h2>Titel</h2>
+      <h3>yo</h3>
+      <img src="./images/jip.jpg" alt="" />
+      <img src="./images/jip.jpg" alt="" />
+      <img src="./images/jip.jpg" alt="" />
+
+    </div>
+  );
+
+  
+};
+
+export default Book;
+
